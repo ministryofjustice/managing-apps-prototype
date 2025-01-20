b6c2ceb4ce74c760638cc01178c357cfc88eb01c
Jen Thomson <hello@jenthomson.com>
2025-01-20 22:33:23 +0000

    Post assessment update of design to reflect lates figma
    Mostly content changes
    Restructure to add a static set of pages for each app
    prototype home page restructure.

--- app/routes/managing-apps-latest.js
+++ app/routes/managing-apps-latest.js
@@ -1,39 +1,30 @@
 module.exports = function (router, content) {
 
   router.post('/staff/latest/applications/log/select-app-type', function (req, res) {
-    res.redirect('/staff/latest/applications/log/details')
-})
-
-
-  router.post('/staff/latest/applications/log/details', function (req, res) {
     // Make a variable and give it the value from 'how-many-balls'
     var appType = req.session.data['appType']
-    // Check whether the variable matches a condition
+
     if (appType == "bh_01") {
       // Send user to ineligible page
-      res.redirect('/staff/latest/applications/log/new-social-pin-contact.html')
+      res.redirect('/staff/latest/applications/log/new-social-pin-contact/prisoner-details.html')
     }
     if (appType == "bh_02") {
-      // Send user to ineligible page
-      res.redirect('/staff/latest/applications/log/new-legal-pin-contact.html')
+      res.redirect('/staff/latest/applications/log/new-legal-pin-contact/prisoner-details.html')
     }
     if (appType == "bh_03") {
-      // Send user to ineligible page
-      res.redirect('/staff/latest/applications/log/emergency-pin-credit.html')
+      res.redirect('/staff/latest/applications/log/emergency-pin-credit/prisoner-details.html')
     }
     if (appType == "bh_04") {
-      // Send user to ineligible page
-      res.redirect('/staff/latest/applications/log/remove-pin-contact.html')
+      res.redirect('/staff/latest/applications/log/remove-pin-contact/prisoner-details.html')
     }
     if (appType == "bh_05") {
-      // Send user to ineligible page
-      res.redirect('/staff/latest/applications/log/swap-vos-pin-credit.html')
+      res.redirect('/staff/latest/applications/log/swap-vos-pin-credit/prisoner-details.html')
     }
     if (appType == "bh_06") {
-      // Send user to ineligible page
-      res.redirect('/staff/latest/applications/log/supply-pin-contacts.html')
+      res.redirect('/staff/latest/applications/log/supply-pin-contacts/prisoner-details.html')
     }
   })
+
 
 
   router.post('/staff/latest/applications/omu/apps/app-19/action', function (req, res) {
