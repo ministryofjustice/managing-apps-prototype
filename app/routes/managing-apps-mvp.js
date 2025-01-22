e60ac3cfe886f259030301da2c9023dffeda726a
Jen Thomson <hello@jenthomson.com>
2025-01-22 15:50:56 +0000

    Creating an MVP version
    So that we are linking to something that will persist, rather than the latest version which will be updated, I am creating an MVP url which we can use the links in for all tickets and designs.

---
+++ app/routes/managing-apps-mvp.js
@@ -0,0 +1,46 @@
+module.exports = function (router, content) {
+
+  router.post('/staff/mvp/applications/log/select-app-type', function (req, res) {
+    // Make a variable and give it the value from 'how-many-balls'
+    var appType = req.session.data['appType']
+
+    if (appType == "bh_01") {
+      // Send user to ineligible page
+      res.redirect('/staff/mvp/applications/log/new-social-pin-contact/prisoner-details.html')
+    }
+    if (appType == "bh_02") {
+      res.redirect('/staff/mvp/applications/log/new-legal-pin-contact/prisoner-details.html')
+    }
+    if (appType == "bh_03") {
+      res.redirect('/staff/mvp/applications/log/emergency-pin-credit/prisoner-details.html')
+    }
+    if (appType == "bh_04") {
+      res.redirect('/staff/mvp/applications/log/remove-pin-contact/prisoner-details.html')
+    }
+    if (appType == "bh_05") {
+      res.redirect('/staff/mvp/applications/log/swap-vos-pin-credit/prisoner-details.html')
+    }
+    if (appType == "bh_06") {
+      res.redirect('/staff/mvp/applications/log/supply-pin-contacts/prisoner-details.html')
+    }
+  })
+
+
+
+  router.post('/staff/mvp/applications/omu/apps/app-19/action', function (req, res) {
+      res.redirect('/staff/mvp/applications/omu/apps/app-19/action-decision')
+  })
+
+  router.post('/staff/mvp/applications/business-hub/apps/app-16/action', function (req, res) {
+      res.redirect('/staff/mvp/applications/business-hub/apps/app-16/action-decision')
+  })
+
+  router.post('/staff/mvp/applications/business-hub/apps/app-1/action', function (req, res) {
+      res.redirect('/staff/mvp/applications/business-hub/apps/app-1/action-decision')
+  })
+
+  router.post('/staff/mvp/applications/omu/apps/app-1/action', function (req, res) {
+      res.redirect('/staff/mvp/applications/omu/apps/app-1/action-decision')
+  })
+
+}
