e4fccf4e0a320db2baadbee5699f55e04ec69bdb
Jen <hello@jenthomson.com>
2025-03-24 23:28:56 +0000

    Added show and hide filter tags

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -1,34 +1,71 @@
 window.GOVUKPrototypeKit.documentReady(() => {
-  function filterTable() {
-    var nameOrNumber = $('#prisoner-autocomplete').val().toLowerCase();
-    
-    var selectedDepartments = $('input[name="departmentFilter"]:checked').map(function() {
-        return $(this).val().toLowerCase();
-    }).get();
-    
-    var selectedTypes = $('input[name="typeFilter"]:checked').map(function() {
-        return $(this).val().toLowerCase();
-    }).get();
 
-    $('#dataTable tbody tr').each(function() {
-        var tags = $(this).data('tags').toLowerCase();
+    $('.moj-filter-tags--group').hide();
 
-        var showRow = (tags.indexOf(nameOrNumber) > -1 || nameOrNumber === '') &&
-                      (selectedDepartments.length === 0 || selectedDepartments.some(dept => tags.indexOf(dept) > -1)) &&
-                      (selectedTypes.length === 0 || selectedTypes.some(type => tags.indexOf(type) > -1));
+    function filterTable() {
+        var prisonerNameNumber = $('#prisoner-autocomplete').val();
+        var nameOrNumber = $('#prisoner-autocomplete').val().toLowerCase();
+        
+        var selectedDepartments = $('input[name="departmentFilter"]:checked').map(function() {
+            return $(this).val();
+        }).get();
+        
+        var selectedTypes = $('input[name="typeFilter"]:checked').map(function() {
+            return $(this).val();
+        }).get();
 
-        $(this).toggle(showRow);
-    });
-}
+        $('#dataTable tbody tr').each(function() {
+            var tags = $(this).data('tags').toLowerCase();
 
-function clearFilters() {
-    $('#nameOrNumberFilter').val('');
-    $('.departmentFilter').prop('checked', false);
-    $('.typeFilter').prop('checked', false);
-    filterTable();
-}
+            var showRow = (tags.indexOf(nameOrNumber) > -1 || nameOrNumber === '') &&
+                          (selectedDepartments.length === 0 || selectedDepartments.some(dept => tags.indexOf(dept.toLowerCase()) > -1)) &&
+                          (selectedTypes.length === 0 || selectedTypes.some(type => tags.indexOf(type.toLowerCase()) > -1));
 
-$('#applyFiltersButton').on('click', filterTable);
-$('#clearFiltersButton').on('click', clearFilters);
+            $(this).toggle(showRow);
+        });
+
+        updateFilterTags(prisonerNameNumber, selectedDepartments, selectedTypes);
+    }
+
+    function updateFilterTags(prisonerNameNumber, selectedDepartments, selectedTypes) {
+        $('#dept-tags').empty();
+        $('#prisoner-tags').empty();
+        $('#type-tags').empty();
+
+        if (prisonerNameNumber) {
+            $('#prisoner-tags').append('<li><a href="#" class="moj-filter__tag">' + prisonerNameNumber + '</a></li>');
+            $('#moj-filter-tags--prisoner').show();
+        } else {
+            $('#moj-filter-tags--prisoner').hide();
+        }
+
+        if (selectedDepartments.length > 0) {
+            selectedDepartments.forEach(function(dept) {
+                $('#dept-tags').append('<li><a href="#" class="moj-filter__tag">' + dept + '</a></li>');
+            });
+            $('#moj-filter-tags--department').show();
+        } else {
+            $('#moj-filter-tags--department').hide();
+        }
+
+        if (selectedTypes.length > 0) {
+            selectedTypes.forEach(function(type) {
+                $('#type-tags').append('<li><a href="#" class="moj-filter__tag">' + type + '</a></li>');
+            });
+            $('#moj-filter-tags--type').show();
+        } else {
+            $('#moj-filter-tags--type').hide();
+        }
+    }
+
+    function clearFilters() {
+        $('#prisoner-autocomplete').val('');
+        $('input[name="departmentFilter"]').prop('checked', false);
+        $('input[name="typeFilter"]').prop('checked', false);
+        filterTable();
+    }
+
+    $('#applyFiltersButton').on('click', filterTable);
+    $('#clearFiltersButton').on('click', clearFilters);
 
 });
