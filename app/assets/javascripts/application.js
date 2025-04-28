89f78c780519d7df6e561827c0df8402f0f77c88
Jen <hello@jenthomson.com>
2025-04-28 16:25:16 +0100

    Adding in the ability to log apps with 4 other prisoner ids

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,13 +37,29 @@ window.GOVUKPrototypeKit.documentReady(() => {
 
   $('a#prisonNumber-lookup').click(function () {
     // alert('this works');
-    var prisonNumber =  $('input#prisonNumber').val();
+    var prisonNumber = $('input#prisonNumber').val();
     console.log(prisonNumber);
     //alert(prisonNumber);
     if (prisonNumber.length > 2) {
-      $('#prisoner-name-row').show();
+        $('#prisoner-name-row').show();
+        if (prisonNumber == 'G4567NO') {
+            $('#prisoner-name-row').text('Prisoner name: Blake, Oliver');
+            $('#prisonerName').val('Blake, Oliver'); // Use .val() instead of .value()
+        } else if (prisonNumber == 'J6789TU') { // Use else if instead of else
+            $('#prisoner-name-row').text('Prisoner name: Gupta, Vikram');
+            $('#prisonerName').val('Gupta, Vikram'); // Use .val() instead of .value()
+        } else if (prisonNumber == 'D2345HI') { // Use else if instead of else
+            $('#prisoner-name-row').text('Prisoner name: Ali, Saeed');
+            $('#prisonerName').val('Ali, Saeed'); // Use .val() instead of .value()
+        } else if (prisonNumber == 'A1234BC') { // Use else if instead of else
+            $('#prisoner-name-row').text('Prisoner name: Wright, Benjamin');
+            $('#prisonerName').val('Wright, Benjamin'); // Use .val() instead of .value()
+        } else {
+            $('#prisoner-name-row').text('Prisoner name: Patel, Taj');
+            $('#prisonerName').val('Patel, Taj'); // Use .val() instead of .value()
+        }
     }
-  })
+});
 
   // Convert spreadsheet date formats to readable dates
   $('td.date').each(function () {
