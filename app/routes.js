72bbc2fdd7c25738d0bdd6ed8c75f6b1a16be2d3
Jen <hello@jenthomson.com>
2025-07-02 16:57:49 +0100

    Added in a single general app prototype

--- app/routes.js
+++ app/routes.js
@@ -15,6 +15,7 @@ require('./routes/managing-apps-1-7.js')(router);
 require('./routes/managing-apps-mvp.js')(router);
 require('./routes/managing-apps-mvp-phase-2.js')(router);
 require('./routes/general-apps.js')(router);
+require('./routes/single-general-app.js')(router);
 require('./routes/admin-latest.js')(router);
 require('./routes/sandbox.js')(router);
 
