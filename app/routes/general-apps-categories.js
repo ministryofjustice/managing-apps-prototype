f55219affe3f104546695bff989a8847281c8fb5
Jen <hello@jenthomson.com>
2025-07-08 11:49:05 +0100

    Updated data for app types and fixed minor issues

--- app/routes/general-apps-categories.js
+++ app/routes/general-apps-categories.js
@@ -1,37 +1,37 @@
 module.exports = function (router, content) {
 
-  router.post('/staff/general-apps-categories/applications/log/select-app-type', function (req, res) {
+  router.post('/staff/general-apps-categories/applications/log/select-app-group', function (req, res) {
     // Make a variable and give it the value from 'how-many-balls'
     var appType = req.session.data['appType']
 
     if (appType == 'cat_00') {
-      res.redirect('/staff/general-apps-categories/applications/log/prisoner-details?appName=app_51&appType=cat_00' )
+      res.redirect('/staff/general-apps-categories/applications/log/prisoner-details?appType=app_51&appType=cat_00' )
     } 
     else {
-      res.redirect('/staff/general-apps-categories/applications/log/select-app-name')
+      res.redirect('/staff/general-apps-categories/applications/log/select-app-type')
     }
   })
 
-  router.post('/staff/general-apps-categories/applications/log/select-app-name', function (req, res) {
+  router.post('/staff/general-apps-categories/applications/log/select-app-type', function (req, res) {
     // Make a variable and give it the value from 'how-many-balls'
-    var appName = req.session.data['appName']
+    var appType = req.session.data['appType']
 
-    if (appName == "app_30") {
+    if (appType == "app_30") {
       res.redirect('/staff/general-apps-categories/applications/log/new-social-pin-contact/prisoner-details')
     }
-    else if (appName == "app_31") {
+    else if (appType == "app_31") {
       res.redirect('/staff/general-apps-categories/applications/log/new-legal-pin-contact/prisoner-details')
     }
-    else if (appName == "app_32") {
+    else if (appType == "app_32") {
       res.redirect('/staff/general-apps-categories/applications/log/emergency-pin-credit/prisoner-details')
     }
-    else if (appName == "app_33") {
+    else if (appType == "app_33") {
       res.redirect('/staff/general-apps-categories/applications/log/swap-vos-pin-credit/prisoner-details')
     }
-   else if (appName == "app_34") {
+   else if (appType == "app_34") {
       res.redirect('/staff/general-apps-categories/applications/log/supply-pin-contacts/prisoner-details')
     }
-    else if (appName == "app_35") {
+    else if (appType == "app_35") {
       res.redirect('/staff/general-apps-categories/applications/log/remove-pin-contact/prisoner-details')
     }
     else {
